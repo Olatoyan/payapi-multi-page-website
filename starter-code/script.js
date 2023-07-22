6b3b1163021fbcaadc8510e5c6a6a151870fec3e
Olatoyan <golatoyan@gmail.com>
2023-07-22 05:28:04 +0100

    finished home page

--- starter-code/script.js
+++ starter-code/script.js
@@ -5,8 +5,6 @@ const closeNav = document.querySelector(".nav-close");
 const nav = document.querySelector(".main-nav");
 
 const displayNav = function () {
-  nav.classList.toggle("mid:hidden");
-  nav.classList.toggle("mid:flex");
   nav.classList.toggle("mid:opacity-0");
   nav.classList.toggle("mid:translate-x-full");
   nav.classList.toggle("mid:invisible");
